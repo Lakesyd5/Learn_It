26d4164ba64a28a2a27e533b2f01be90dff080a9
Adeleke <adeleoje@gmail.com>
2024-01-05 01:58:11 +0100

    onboarding cubit done with test

--- test/src/on_boarding/presentation/cubit/cubit/on_boarding_cubit_test.dart
+++ test/src/on_boarding/presentation/cubit/cubit/on_boarding_cubit_test.dart
@@ -1,4 +1,7 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:learn_it/core/errors/failures.dart';
 import 'package:learn_it/src/on_boarding/domain/usecases/cache_first_timer.dart';
 import 'package:learn_it/src/on_boarding/domain/usecases/check_if_user_is_firsttimer.dart';
 import 'package:learn_it/src/on_boarding/presentation/cubit/cubit/on_boarding_cubit.dart';
@@ -12,27 +15,107 @@ class MockCheckIfUserIsFirstTimer extends Mock
 void main() {
   late CacheFirstTimer cacheFirstTimer;
   late CheckIfUserIsFirstTimer checkIfUserIsFirstTimer;
-  late OnBoardingCubit onBoardingCubit;
+  late OnBoardingCubit cubit;
 
   setUp(() {
     cacheFirstTimer = MockCacheFirstTimer();
     checkIfUserIsFirstTimer = MockCheckIfUserIsFirstTimer();
-    onBoardingCubit = OnBoardingCubit(
+    cubit = OnBoardingCubit(
       cacheFirstTimer: cacheFirstTimer,
       checkIfUserIsFirstTimer: checkIfUserIsFirstTimer,
     );
   });
 
+  const tFailure = CacheFailure(
+    message: 'Insufficient storage Permission',
+    statusCode: 4032,
+  );
+
   test('initial state should be [OnbordingInitial]', () async {
-    expect(onBoardingCubit.state, OnBoardingInitial());
+    expect(cubit.state, OnBoardingInitial());
   });
 
   group('cacheFirstTimer', () {
     blocTest<OnBoardingCubit, OnBoardingState>(
       'should emit [CachingFirstTimer, UserCached] when successful.',
-      build: () => OnBoardingCubitBloc(),
-      act: (bloc) => bloc.add(MyEvent),
-      expect: () => const <OnBoardingCubitState>[MyState],
+      build: () {
+        when(() => cacheFirstTimer()).thenAnswer(
+          (_) async => const Right(null),
+        );
+
+        return cubit;
+      },
+      act: (cubit) => cubit.cacheFirstTimer(),
+      expect: () => const [
+        CachingFirstTimer(),
+        UserCached(),
+      ],
+      verify: (_) {
+        verify(() => cacheFirstTimer()).called(1);
+        verifyNoMoreInteractions(cacheFirstTimer);
+      },
     );
-   });
+
+    blocTest<OnBoardingCubit, OnBoardingState>(
+      'should emit [CachingFirstTimer, OnBoardingError] when unsuccessful',
+      build: () {
+        when(() => cacheFirstTimer()).thenAnswer(
+          (_) async => const Left(tFailure),
+        );
+
+        return cubit;
+      },
+      act: (cubit) => cubit.cacheFirstTimer(),
+      expect: () => [
+        const CachingFirstTimer(),
+        OnboardingError(tFailure.message),
+      ],
+      verify: (_) {
+        verify(() => cacheFirstTimer()).called(1);
+        verifyNoMoreInteractions(cacheFirstTimer);
+      },
+    );
+  });
+
+  group('checkIfUserIsFirsTimer', () {
+    blocTest<OnBoardingCubit, OnBoardingState>(
+      'should emit [CheckingIfUserIsFirstTimer, OnboardingStatus] when successful.',
+      build: () {
+        when(() => checkIfUserIsFirstTimer()).thenAnswer(
+          (_) async => const Right(false),
+        );
+
+        return cubit;
+      },
+      act: (cubit) => cubit.checkIfUserIsFirstTimer(),
+      expect: () => const [
+        CheckingIfUserIsFirstTimer(),
+        OnboardingStatus(isFirstTimer: false),
+      ],
+      verify: (_) {
+        verify(() => checkIfUserIsFirstTimer()).called(1);
+        verifyNoMoreInteractions(checkIfUserIsFirstTimer);
+      },
+    );
+
+    blocTest<OnBoardingCubit, OnBoardingState>(
+      'should emit [CheckingIfUserIsFirstTimer, OnboardingError]',
+      build: () {
+        when(() => checkIfUserIsFirstTimer()).thenAnswer(
+          (_) async => const Left(tFailure),
+        );
+
+        return cubit;
+      },
+      act: (cubit) => cubit.checkIfUserIsFirstTimer(),
+      expect: () => const [
+        CheckingIfUserIsFirstTimer(),
+        OnboardingStatus(isFirstTimer: true),
+      ],
+      verify: (bloc) {
+        verify(() => checkIfUserIsFirstTimer()).called(1);
+        verifyNoMoreInteractions(checkIfUserIsFirstTimer);
+      },
+    );
+  });
 }
