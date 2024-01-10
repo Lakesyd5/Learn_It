edddd75f401fa8aa0bdeaa0f550944015e467b4f
Adeleke <adeleoje@gmail.com>
2024-01-10 23:17:55 +0100

    auth remote resource

--- lib/src/auth/data/datasources/auth_remote_data_source.dart
+++ lib/src/auth/data/datasources/auth_remote_data_source.dart
@@ -1,9 +1,14 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:learn_it/core/enums/update_user.dart';
 import 'package:learn_it/core/errors/exception.dart';
+import 'package:learn_it/core/utils/constants.dart';
+import 'package:learn_it/core/utils/typedefs.dart';
 import 'package:learn_it/src/auth/data/model/user_model.dart';
 
 abstract class AuthRemoteDataSource {
@@ -58,23 +63,138 @@ class AuthRemoteDataSourceImpl extends AuthRemoteDataSource {
     required String password,
   }) async {
     try {
-      _authClient.signInWithEmailAndPassword(email: email, password: password);
-    } catch (e) {
-      
+      final result = await _authClient.signInWithEmailAndPassword(
+          email: email, password: password);
+      final user = result.user;
+
+      if (user == null) {
+        throw const ServerException(
+            message: 'Please try again later', statusCode: 'Unknown Error');
+      }
+
+      var userData = await _getUserData(user.uid);
+
+      if (userData.exists) {
+        return LocalUserModel.fromMap(userData.data()!);
+      }
+
+      // upload the user
+      await _setUserData(user, email);
+
+      userData = await _getUserData(user.uid);
+      return LocalUserModel.fromMap(userData.data()!);
+    } on FirebaseAuthException catch (e) {
+      throw ServerException(
+          message: e.message ?? 'Unknown error', statusCode: e.code);
+    } on ServerException {
+      rethrow;
+    } catch (e, s) {
+      debugPrintStack(stackTrace: s);
+      throw ServerException(message: e.toString(), statusCode: '505');
     }
   }
 
   @override
-  Future<void> signUp(
-      {required String email,
-      required String password,
-      required String fullName}) {
-    throw UnimplementedError();
+  Future<void> signUp({
+    required String email,
+    required String password,
+    required String fullName,
+  }) async {
+    try {
+      final userCred = await _authClient.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      await userCred.user?.updateDisplayName(fullName);
+      await userCred.user?.updatePhotoURL(kDefaultAvatar);
+      await _setUserData(_authClient.currentUser!, email);
+    } on FirebaseAuthException catch (e) {
+      throw ServerException(
+          message: e.message ?? 'Unknown error', statusCode: e.code);
+    } on ServerException {
+      rethrow;
+    } catch (e, s) {
+      debugPrintStack(stackTrace: s);
+      throw ServerException(message: e.toString(), statusCode: '505');
+    }
   }
 
   @override
-  Future<void> updateUser({required UpdateUserAction action, userData}) {
-    // TODO: implement updateUser
-    throw UnimplementedError();
+  Future<void> updateUser({required UpdateUserAction action, userData}) async {
+    try {
+      switch (action) {
+        case UpdateUserAction.email:
+          await _authClient.currentUser?.updateEmail(userData as String);
+          await _updateUser({'email': userData});
+
+        case UpdateUserAction.displayName:
+          await _authClient.currentUser?.updateDisplayName(userData as String);
+          await _updateUser({'fullName': userData});
+
+        case UpdateUserAction.profilePic:
+          final ref = _dbClient
+              .ref()
+              .child('profile_pics/${_authClient.currentUser?.uid}');
+
+          await ref.putFile(userData as File);
+          final url = await ref.getDownloadURL();
+          await _authClient.currentUser?.updatePhotoURL(url);
+          await _updateUser({'profilePic': url});
+
+        case UpdateUserAction.password:
+          if (_authClient.currentUser?.email == null) {
+            throw const ServerException(
+              message: 'User does not exist',
+              statusCode: 'Insufficient Permission',
+            );
+          }
+          final newData = jsonDecode(userData as String) as DataMap;
+
+          await _authClient.currentUser?.reauthenticateWithCredential(
+            EmailAuthProvider.credential(
+              email: _authClient.currentUser!.email!,
+              password: newData['oldPassword'] as String,
+            ),
+          );
+
+          await _authClient.currentUser?.updatePassword(
+            newData['newPassword'] as String,
+          );
+
+        case UpdateUserAction.bio:
+          await _updateUser({'bio': userData as String});
+      }
+    } on FirebaseException catch (e) {
+      throw ServerException(
+        message: e.message ?? 'Error Occurred',
+        statusCode: e.code,
+      );
+    } catch (e, s) {
+      debugPrintStack(stackTrace: s);
+      throw ServerException(message: e.toString(), statusCode: '505');
+    }
+  }
+
+  Future<DocumentSnapshot<DataMap>> _getUserData(String uid) async {
+    return _cloudStoreClient.collection('users').doc(uid).get();
+  }
+
+  Future<void> _setUserData(User user, String fallbackEmail) async {
+    await _cloudStoreClient.collection('users').doc(user.uid).set(
+          LocalUserModel(
+            uid: user.uid,
+            email: user.email ?? fallbackEmail,
+            points: 0,
+            fullName: user.displayName ?? '',
+          ).toMap(),
+        );
+  }
+
+  Future<void> _updateUser(DataMap data) async {
+    await _cloudStoreClient
+        .collection('users')
+        .doc(_authClient.currentUser?.uid)
+        .update(data);
   }
 }
---
+++ test/src/auth/data/datasources/auth_remote_data_source_test.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockFirebaseAuth extends Mock implements FirebaseStorage {}
+
+void main() {
+  test('auth remote data source ...', () async {
+    
+  });
+}
